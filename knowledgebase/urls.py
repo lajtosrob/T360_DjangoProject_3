73aca630ab89eb9ae23f06437dbd32689c37f74d
lajtosrob <lajtosrobert@freemail.hu>
2023-07-04 14:57:27 +0200

    22. Tudáskezelő - tag nézetek

--- knowledgebase/urls.py
+++ knowledgebase/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from knowledgebase import views
 
 urlpatterns = [
-    path('knowledge/create', views.KnowledgeCreateView.as_view(), name='knowledge-create'),
+    path('knowledge/create/', views.KnowledgeCreateView.as_view(), name='knowledge-create'),
     path('knowledge/view/<int:pk>', views.KnowledgeDetailView.as_view(), name='knowledge-view'),
     path('knowledge/update/<int:pk>', views.KnowledgeUpdateView.as_view(), name='knowledge-update'),
     path('knowledge/delete/<int:pk>', views.KnowledgeDeleteView.as_view(), name='knowledge-delete'),
+    path('tag/create/', views.TagCreateView.as_view(), name='tag-create'),
+    path('tag/update/<int:pk>', views.TagUpdateView.as_view(), name='tag-update'),
+    path('tag/delete/<int:pk>', views.TagDeleteView.as_view(), name='tag-delete'),
 ]
--- knowledgebase/views.py
+++ knowledgebase/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
-from knowledgebase.models import Knowledge
+from knowledgebase.models import Knowledge, Tag
 
 class KnowledgeCreateView(CreateView):
     model = Knowledge
@@ -18,4 +18,21 @@ class KnowledgeDeleteView(DeleteView):
 
 class KnowledgeDetailView(DetailView):
     model = Knowledge
+    fields = '__all__'
+
+class TagCreateView(CreateView):
+    model = Tag
+    fields = '__all__'
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    fields = '__all__'
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    fields = '__all__'
+    success_url = '/'
+
+class TagDetailView(DetailView):
+    model = Tag
     fields = '__all__'
