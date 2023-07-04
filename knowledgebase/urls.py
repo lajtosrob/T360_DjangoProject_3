c784a0b0b5daf0f7249d1828f7a3aa5e12fc4ce8
lajtosrob <lajtosrobert@freemail.hu>
2023-07-04 14:36:52 +0200

    19. UpdateView használata

--- knowledgebase/urls.py
+++ knowledgebase/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from knowledgebase import views
 
 urlpatterns = [
-    path('knowledge/create', views.KnowledgeCreateView.as_view(), name='knowledge-create')
+    path('knowledge/create', views.KnowledgeCreateView.as_view(), name='knowledge-create'),
+    path('knowledge/update/<int:pk>', views.KnowledgeUpdateView.as_view(), name='knowledge-update')
 ]
--- knowledgebase/views.py
+++ knowledgebase/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from knowledgebase.models import Knowledge
 
 class KnowledgeCreateView(CreateView):
+    model = Knowledge
+    fields = '__all__'
+
+class KnowledgeUpdateView(UpdateView):
     model = Knowledge
     fields = '__all__'
 
